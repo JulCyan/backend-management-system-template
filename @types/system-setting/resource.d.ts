f2f82d07eaa57f0bf5d4705bc4be75516261a216
JulCyan <17688018176@163.com>
2021-01-13 15:05:56 +0800

    Config auto generate .d.ts

---
+++ @types/system-setting/resource.d.ts
@@ -0,0 +1,9 @@
+declare module '@/api' {
+  export const resourceListGetAllEnable: IRequestFunc
+  export const resourceList: IRequestFunc
+  export const resourceDetails: IRequestFunc
+  export const resourceAdd: IRequestFunc
+  export const resourceEdit: IRequestFunc
+  export const resourceDel: IRequestFunc
+  export const verifyPerKey: IRequestFunc
+}
---
+++ @types/system-setting/role.d.ts
@@ -0,0 +1,8 @@
+declare module '@/api' {
+  export const roleGetAll: IRequestFunc
+  export const roleList: IRequestFunc
+  export const roleAdd: IRequestFunc
+  export const roleDetails: IRequestFunc
+  export const roleEdit: IRequestFunc
+  export const roleDel: IRequestFunc
+}
