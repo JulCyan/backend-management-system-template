2f5a9f7c51d25cb5e6b6a3eb9c1148f3334b5eeb
JulCyan <17688018176@163.com>
2021-03-15 17:08:23 +0800

    Feat ThemePicker:ChangeColor

--- src/layout/components/Settings/index.vue
+++ src/layout/components/Settings/index.vue
@@ -2,7 +2,7 @@
   <div class="drawer-container">
     <div>
       <h3 class="drawer-title">
-        {{ $t('settings.title') }}
+        {{ $t('settings.desc') }}
       </h3>
 
       <div class="drawer-item">
